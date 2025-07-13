14b064e90e4fa257627b1907e8f62c32a8dfcf5b
Michael Colledge <CenPenSystem@outlook.com>
2025-07-13 12:52:13 -0400

    remove double lines of code

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+
 
 
 // === PostgreSQL Setup ===
