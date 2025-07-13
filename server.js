ae93f4c23a976b26d773e30b6bc44ace07cd6f2b
Michael Colledge <CenPenSystem@outlook.com>
2025-07-13 12:38:53 -0400

    port configuration

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 
 // === PostgreSQL Setup ===
 const pool = new Pool({
